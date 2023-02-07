d19cbdf2bc7f5a1d1201eceb7a3a14bbaf9f345e
Robert So <robert.so@crowdbotics.com>
2023-02-06 19:24:19 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "robdevenv-38686",
   url: "https://robdevenv-38686.botics.co",
   api: "https://robdevenv-38686.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
